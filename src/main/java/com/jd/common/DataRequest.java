c4ae8b9e13b5db209b129110f2768b470b8b007a
86136ljhljhlfdfds <335801354@qq.com>
2019-08-13 22:22:06 +0800

    request response规范

---
+++ src/main/java/com/jd/common/DataRequest.java
@@ -0,0 +1,30 @@
+package com.jd.common;
+
+import java.io.Serializable;
+
+/**
+ *携带数据的请求
+ */
+public class DataRequest<T> extends MessageRequest {
+    private static final long serialVersionUID = 9009536746362822146L;
+
+    /**
+     * 携带数据
+     */
+    private T data;
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "DataRequest{" +
+                "data=" + data +
+                '}';
+    }
+}
